4880fbf4b1e187e4448eb03c47486923e7288197
Sahil wagh <Sahilwagh142@gmail.com>
2023-12-27 13:28:18 +0530

    babel update

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,17 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
+      "react-native-reanimated/plugin",
+    ],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      }],
     ],
   };
 };
